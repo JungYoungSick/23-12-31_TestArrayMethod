4af992d49d9cfe8fbd0fe4d405780fa08c6f26b4
JungYoungSick <junfas1216@gmail.com>
2023-11-09 17:49:35 +0900

    Q1 예제문제 풀어봄

--- Q1.js
+++ Q1.js
@@ -5,3 +5,10 @@ console.log(myArray)
 // 기존 배열에 값이 추가 되었다.(4,5,6)
 console.log(newLength)
 // (6)
+
+const fruite = []
+
+fruite.push("딸기");
+fruite.push("바나나");
+fruite.push("배");
+console.log(fruite);
