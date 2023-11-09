e372f06c9bc41955f6079ac1a36bbab906d67d26
JungYoungSick <junfas1216@gmail.com>
2023-11-09 17:56:41 +0900

    Q2 예제문제 풀어봄

--- Q2.js
+++ Q2.js
@@ -4,3 +4,11 @@ console.log(myArray);
 // 기존의 값에서 마지막 배열의 값이 삭제되었다.
 console.log(lastElement);
 // 삭제된 마지막 배열의 값을 알려준다.
+
+const list = [
+  { name: "jung" },
+  { age: 18 }
+]
+const lastList = list.pop();
+console.log(list);
+console.log(lastList);
