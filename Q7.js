7c1b0540d83505e8770210583dfe075b9fc3350a
JungYoungSick <junfas1216@gmail.com>
2023-11-11 21:16:46 +0900

    Q7 해결

--- Q7.js
+++ Q7.js
@@ -1,1 +1,5 @@
 // 명단에서 성씨가 ‘김’씨인 학생의 이름을 찾아 출력하는 코드를 작성하세요.
+const fruite = ["딸기", "바나나", "수박", "메론", "포도", "청사과", "청포도"]
+
+const fruiteAnd = fruite.filter(fruit => fruit.includes('청'));
+console.log(fruiteAnd)
