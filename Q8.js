38d45528076e186bcd7a61e368e9b1341098c96d
JungYoungSick <junfas1216@gmail.com>
2023-11-11 21:22:20 +0900

    Q8 문제 해결

--- Q8.js
+++ Q8.js
@@ -1,1 +1,7 @@
 // 기초데이터(studentList, pokemonList) 두 개의 명단을 합쳐 전체 학생 명단을 만들려고 합니다. 두 명단을 결합하는 코드를 작성하세요
+const fruite = ["딸기", "바나나", "수박", "메론", "포도", "청사과", "청포도"]
+const animal = ["토끼", "말", "강아지", "고양이"]
+
+const animalFruite = fruite.concat(animal);
+
+console.log(animalFruite)
