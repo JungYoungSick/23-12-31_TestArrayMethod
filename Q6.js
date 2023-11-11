560f7663a2bbcb7169145c6049b0916b2fcc3874
JungYoungSick <junfas1216@gmail.com>
2023-11-11 19:38:08 +0900

    Q6 해결 못함

--- Q6.js
+++ Q6.js
@@ -1,3 +1,24 @@
 //명단에 같은 이름(성씨제외)을 가진 학생이 두 명 있습니다. 이 중 마지막에 있는 학생의 위치를 찾아 출력하는 코드를 작성하세요.
+const fruite = ["딸기", "바나나", "수박", "메론", "포도", "사과", "사과"]
 
+const fruitename = '사과';
 
+let myfruite = [];
+for (let i = 0; i < fruite.length; i++) {
+  if (fruite[i].split('') === fruitename) {
+    myfruite.push(i);
+  }
+}
+
+let lastApple;
+if (myfruite.length > 0) {
+  lastApple = myfruite[myfruite.length - 1];
+} else {
+  lastApple = -1;
+}
+
+if (lastApple >= 0) {
+  console.log(`마지막 ${myfruite}사과의 위치: ${lastApple}`);
+} else {
+  console.log(`${myfruite}는 없습니다.`)
+}
