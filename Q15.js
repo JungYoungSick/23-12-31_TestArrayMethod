60e8d805de4fb6e6f55caa00c8dc27d62aa79632
JungYoungSick <junfas1216@gmail.com>
2023-11-11 22:57:44 +0900

    Q15 작성 완료

--- Q15.js
+++ Q15.js
@@ -1,1 +1,11 @@
 // 명단에 '홍'으로 시작하는 학생이 한 명이라도 있는지 확인하는 코드를 작성하세요.
+
+const fruit = ["딸기", "바나나", "수박", "메론", "포도", "청사과", "청포도", "키위"]
+
+const start = fruit.some(name => name.startsWith("청"))
+
+if (start) {
+  console.log("청으로 시작하는 과일이 있습니다.")
+} else {
+  console.log("청으로 시작하는 과일이 없습니다..")
+}
