732641805bbbe71ca30fd58d33c69f0b63a32ca4
JungYoungSick <junfas1216@gmail.com>
2023-11-11 22:19:25 +0900

    Q12 작성 완료

--- Q12.js
+++ Q12.js
@@ -1,1 +1,8 @@
 // 명단에서 마지막 학생이 전학을 갔습니다. 이 학생을 명단에서 제거하고, 그 학생의 이름을 출력하는 코드를 작성하세요.
+
+const fruit = ["딸기", "바나나", "수박", "메론", "포도", "청사과", "청포도"]
+const lastFruits = fruit.pop()
+
+console.log(lastFruits)
+console.log(fruit)
+
