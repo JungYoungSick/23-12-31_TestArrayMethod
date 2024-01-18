b98b4569a8a09443d6c29ffa9dc8c554f2189b64
JungYoungSick <junfas1216@gmail.com>
2024-01-18 23:09:01 +0900

    숫자추가

--- push.js
+++ push.js
@@ -3,3 +3,9 @@ console.log('기존배열', fruite)
 
 fruite.push('오렌지')
 console.log('새로운 배열', fruite)
+
+fruite.push('당근', '오이');
+console.log('추가된 배열', fruite)
+
+fruite.push(1, 2, 3, 4, 5)
+console.log('추가된 숫자', fruite)
