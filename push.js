dff1cc8e84efa1a9dce132e102833324047fa371
JungYoungSick <junfas1216@gmail.com>
2024-01-18 23:01:44 +0900

    push 기존배열

--- push.js
+++ push.js
@@ -1,14 +1,3 @@
-const myArray = [1, 2, 3]
-const newLength = myArray.push(4, 5, 6);
+let fruite = ['사과', '바나나', '수박', '포도']
+console.log('기존배열', fruite)
 
-console.log(myArray)
-// 기존 배열에 값이 추가 되었다.(4,5,6)
-console.log(newLength)
-// (6)
-
-const fruite = []
-
-fruite.push("딸기");
-fruite.push("바나나");
-fruite.push("배");
-console.log(fruite);
