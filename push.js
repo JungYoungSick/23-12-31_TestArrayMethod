30cdb620696cf9344ebd071e93eee7a8838fff38
JungYoungSick <junfas1216@gmail.com>
2024-01-18 23:12:21 +0900

    Q1 해결

--- push.js
+++ push.js
@@ -9,3 +9,11 @@ console.log('추가된 배열', fruite)
 
 fruite.push(1, 2, 3, 4, 5)
 console.log('추가된 숫자', fruite)
+
+
+// Q1 = 색상추가
+let color = ['초록', '주황'];
+color.push('빨강', '핑크', '보라')
+
+console.log('추가색상', color)
+//! 색상은 추가한 순서대로 등록된다.
