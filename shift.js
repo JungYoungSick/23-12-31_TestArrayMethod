df7dc3bccabf001a6887bc4c457a6755eac556b2
JungYoungSick <junfas1216@gmail.com>
2024-01-22 02:29:16 +0900

    아침식사 메뉴를 하나씩 출력시키는 shift()메서드 예제함수

--- shift.js
+++ shift.js
@@ -3,3 +3,18 @@ const firstAraay = myArray.shift();
 
 console.log(myArray);
 console.log(firstAraay);
+
+const fruites = ["사과", '바나나', "딸기"]
+const removedElement = fruites.shift();
+
+console.log("제거된 요소 : ", removedElement);
+console.log("변경된 배열 : ", fruites);
+
+const breackfastMenu = ["계란", "토스트", "우유", "오렌지 주스"];
+
+while (breackfastMenu.length > 0) {
+  const food = breackfastMenu.shift();
+  console.log(`아침에 먹을 음식 : ${food}`);
+}
+
+console.log("아침식사가 끝났습니다!");
