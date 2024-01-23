2e5405f717d4de8e91f856865b45c901cc7baf0a
JungYoungSick <junfas1216@gmail.com>
2024-01-23 21:07:29 +0900

    unshift메서드의 기초

--- unshift.js
+++ unshift.js
@@ -2,3 +2,7 @@
 const fruite = ["딸기", "바나나", "수박", "메론", "포도"]
 const reverseFruite = fruite.reverse();
 console.log(reverseFruite)
+
+let myArray = [2, 3, 4];
+myArray.unshift(1);
+console.log(myArray);
